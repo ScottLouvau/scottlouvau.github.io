1e354171173d19aaf4dcb2a9c30d01ea3ec0e655
Scott Louvau <slouvau@hotmail.com>
2024-05-22 20:17:17 -0700

    Wordle Analyze PWA attempt

---
+++ static/wordle-analyze/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "wordle-wasm-v1";
+const CACHE_URLS = [
+    ".",
+    "index.html",
+    "wordle-analyze-a.png",
+    "data/v12.txt",
+    "data/answers.txt",
+    "data/valid.txt",
+    "pkg/wordle_wasm_bg.wasm",
+    "pkg/wordle_wasm.js",
+];
+
+async function cacheResources() {
+    const cache = await caches.open(CACHE_NAME);
+    return cache.addAll(CACHE_URLS);
+}
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(cacheResources());
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
