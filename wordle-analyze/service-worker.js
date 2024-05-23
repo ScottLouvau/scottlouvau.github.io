1871ffa3e5b0927953ce36764250d50a791671c0
ScottLouvau <ScottLouvau@users.noreply.github.com>
2024-05-23 04:47:29 +0000

    deploy: f2bcbb7d901ce8a2a44448449c755bf2af071f43

--- wordle-analyze/service-worker.js
+++ wordle-analyze/service-worker.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = "wordle-wasm-v1";
 const CACHE_URLS = [
     "./",
+    "./app.webmanifest",
     "./index.html",
     "./wordle-analyze-a.png",
     "./data/v12.txt",
@@ -15,14 +16,28 @@ async function cacheResources() {
     return cache.addAll(CACHE_URLS);
 }
 
+async function cacheFirst(request) {
+    const cachedResponse = await caches.match(request);
+    if (cachedResponse) { 
+        return cachedResponse; 
+    }
+
+    try {
+        const networkResponse = await fetch(request);
+        if (networkResponse.ok) {
+            const cache = await caches.open(CACHE_NAME);
+            cache.put(request, networkResponse.clone());
+        }
+        return networkResponse
+    } catch (error) {
+        return Response.error();
+    }
+}
+
 self.addEventListener("install", (event) => {
     event.waitUntil(cacheResources());
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+    event.respondWith(cacheFirst(event.request));
 });
